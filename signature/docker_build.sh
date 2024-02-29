325131e555c1b981377be0090198bc1ffdcf9fd7
zaqmond <zaqmond@yandex.ru>
2024-02-29 17:24:12 +0300

    last sign

--- signature/docker_build.sh
+++ signature/docker_build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#флаг запуска контейнера после сборки
+# флаг запуска контейнера после сборки
 long=false
+# флаг возвращения к дефолтным настройкам
+default=false
 
 CONTAINER_NAME="signvault"
 TIME_TO_SLEEP=10
@@ -12,22 +14,28 @@ while [ "$#" -gt 0 ]; do
             long=true
             shift
             ;;
+        -d|--default)
+            default=true
+            shift
+            ;;
     esac
 done
 
 cd /
-# запускаем демон докер если он не запущен и паралельно ждем "$TIME_TO_SLEEP" секунд, для полного запуска
-dockerd & sleep "$TIME_TO_SLEEP"
-# останавливаем все докер контейнеры
-docker container stop $(docker container ls -q)
-# удаляем все докер контейнеры
-docker rm $(docker ps -a -q)
-# загружаем образ vault контейнера для запуска
-docker load -i "$CONTAINER_NAME".tar & sleep "$TIME_TO_SLEEP"
-# запускаем контейнер vault
-docker run -p 8200:8200 --name "$CONTAINER_NAME" --rm -d signvault & sleep "$TIME_TO_SLEEP"
-# распечатываем базу данных внутри vault
-docker exec "$CONTAINER_NAME" ./start.sh & sleep "$TIME_TO_SLEEP"
+if [ "$default" = true ]; then
+    # запускаем демон докер если он не запущен и паралельно ждем "$TIME_TO_SLEEP" секунд, для полного запуска
+    dockerd & sleep "$TIME_TO_SLEEP"
+    # останавливаем все докер контейнеры
+    docker container stop $(docker container ls -q)
+    # удаляем все докер контейнеры
+    docker rm $(docker ps -a -q)
+    # загружаем образ vault контейнера для запуска
+    docker load -i "$CONTAINER_NAME".tar && sleep "$TIME_TO_SLEEP"
+    # запускаем контейнер vault
+    docker run -p 8200:8200 --name "$CONTAINER_NAME" --rm -d signvault & sleep "$TIME_TO_SLEEP"
+    # распечатываем базу данных внутри vault
+    docker exec "$CONTAINER_NAME" ./start.sh & sleep "$TIME_TO_SLEEP"
+fi
 # извлекаем пароль из базы данных в файл
 if [ "$long" = true ]; then
 # Сборка с ключем 256 или 512
@@ -36,7 +44,7 @@ else
     ./extraction_vault $(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$CONTAINER_NAME") 8200 > /builds/bmc/bmc_src/key/privatekey
 fi
 # останавливаем контейнер vault
-docker stop "$CONTAINER_NAME"
+# docker stop "$CONTAINER_NAME"
 # переходим в дерикторию с проектом
 cd /builds/bmc/bmc_src/
 
@@ -46,7 +54,3 @@ if [ "$long" = true ]; then
 else
     ./docker_build.sh sign $DIR_NAME
 fi
-
-cp key/publickey ./
-cp key/readme.txt ./
-cp key/privatekey ./
--- signature/forvault/buildDockerfile.sh
+++ signature/forvault/buildDockerfile.sh
@@ -2,13 +2,13 @@
 
 # Переменные для настроек сборки forvault
 DOCKERFILE_PATH="."  # Путь к Dockerfile
-DOCKER_IMAGE="forvault"    # Имя и тег для собираемого образа
-TAG="latest"
+DOCKER_IMAGE="forvault"    # Имя для собираемого образа
+TAG="latest"    # Тег для собираемого образа
 
 ## Переменные для настроек сборки signvault
 DOCKERFILE_PATH_SIGNVAULT="."  # Путь к Dockerfile
 DOCKER_IMAGE_SIGNVAULT="signvault"    # Имя и тег для собираемого образа
-TAG_SIGNVAULT="latest"
+TAG_SIGNVAULT="latest" # Тег для собираемого образа
 
 #флаг запуска контейнера после сборки
 start=false
@@ -19,16 +19,23 @@ while [ "$#" -gt 0 ]; do
             start=true
             shift
             ;;
+        *)
+            TAG="$1"
+            shift 
+            ;;
     esac
 done
 
-
 # Сборка extraction_vault
 pip install pyinstaller
-export PATH="$PATH:/home/$(whoami)/.local/bin" 
+export PATH="$PATH:/home/$(whoami)/.local/bin"
+pip install hvac
 pyinstaller --onefile extraction_vault.py && cp dist/extraction_vault ./ && rm -rf build/ dist/ extraction_vault.spec 
 
 cd ./images/signvault
+
+# без этого, если БД была редактирована, при запуске контейнера внутри контейнера будет падать в ошибку 
+sudo chmod -R 777 vault/
 
 # Сборка Docker signvault
 docker build -t "$DOCKER_IMAGE_SIGNVAULT":"$TAG_SIGNVAULT" "$DOCKERFILE_PATH_SIGNVAULT"
@@ -43,7 +50,7 @@ cd ..
 # Сборка Docker forvault
 docker build -t "$DOCKER_IMAGE":"$TAG" "$DOCKERFILE_PATH"
 
-#Удаление ненужных фалов
+# #Удаление ненужных фалов
 rm -rf extraction_vault images/.docker_temp_* images/signvault.tar
 
 if [ "$start" = true ]; then
